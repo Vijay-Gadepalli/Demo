68bbb7568281772bff8b22e39f3c6b63ebae3981
Vijay-Gadepalli <38218814+Vijay-Gadepalli@users.noreply.github.com>
2018-08-16 17:01:14 +0530

    New code

---
+++ src/test/java/com/qa/testcases/TestContactsPage.java
@@ -0,0 +1,7 @@
+package com.qa.testcases;
+
+import com.qa.testbase.TestBase;
+
+public class TestContactsPage extends TestBase {
+
+}
--- src/test/java/com/qa/testcases/TestHomePage.java
+++ src/test/java/com/qa/testcases/TestHomePage.java
@@ -1,7 +1,64 @@
 package com.qa.testcases;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.pages.ContactsPage;
+import com.qa.pages.DealsPage;
+import com.qa.pages.HomePage;
+import com.qa.pages.LoginPage;
 import com.qa.testbase.TestBase;
 
 public class TestHomePage extends TestBase {
-
+	LoginPage loginpage;
+	HomePage homepage;
+	DealsPage dealspage;
+	ContactsPage contactspage;
+	
+	public TestHomePage() {
+		super();
+	}
+		
+	@BeforeMethod
+	public void set_up() throws Exception
+	{
+		intilization();
+		loginpage= new LoginPage();
+		homepage = loginpage.Login(prop.getProperty("username"), prop.getProperty("password"));		
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Test
+	public void Username_label_test()
+	{
+		boolean flag= homepage.username_label_check();
+		Assert.assertTrue(flag);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Test
+	public void click_on_contacts_link_test()
+	{
+		contactspage= homepage.click_on_contacts_link();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Test
+	public void click_on_deals_link()
+	{
+		dealspage=homepage.click_on_deals_link();
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@AfterMethod
+	public void tear_down()
+	{
+		driver.quit();
+	}	
 }
