6ad2adbeed543118b84ad20adc291bf3f4de0fc1
Vijay-Gadepalli <38218814+Vijay-Gadepalli@users.noreply.github.com>
2018-08-16 23:30:54 +0530

    new code-16th August

--- src/main/java/com/qa/utilities/Utilities.java
+++ src/main/java/com/qa/utilities/Utilities.java
@@ -1,11 +1,28 @@
 package com.qa.utilities;
 
+import org.openqa.selenium.WebDriver;
+
 import com.qa.testbase.TestBase;
 
 public class Utilities extends TestBase{
+	
+	
+						//Switch To Frame
+	
 	public static void switch_to_frame(String frame_name)
 	{
 		driver.switchTo().frame(frame_name);
+	}
+	
+						// Capture screen shot for failure
+	public static String captureScreenshotForFailureTest(WebDriver driver, String testname )
+	{
+		
+		
+		String final_destination=null;
+		return final_destination;
+		
+		
 	}
 
 }
