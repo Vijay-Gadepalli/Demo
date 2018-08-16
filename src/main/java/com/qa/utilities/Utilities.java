f5118a112bba29b6a1579405e7a4810e0d502683
Vijay-Gadepalli <38218814+Vijay-Gadepalli@users.noreply.github.com>
2018-08-16 17:03:01 +0530

    New code

--- src/main/java/com/qa/utilities/Utilities.java
+++ src/main/java/com/qa/utilities/Utilities.java
@@ -1,5 +1,11 @@
 package com.qa.utilities;
 
-public class Utilities {
+import com.qa.testbase.TestBase;
+
+public class Utilities extends TestBase{
+	public static void switch_to_frame(String frame_name)
+	{
+		driver.switchTo().frame(frame_name);
+	}
 
 }
